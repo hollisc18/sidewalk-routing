4114355c8e2f1294363ca9e4ece394048faa28ac
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-10-15 14:16:08 -0400

    Update sidewalk-navigation.py

--- sidewalk-navigation.py
+++ sidewalk-navigation.py
@@ -60,6 +60,69 @@ sidewalk_json = edges_gdf.to_json()
 folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
 
 user_input = st.text_area("Enter adress", "")
+folium_static(mapCville)
+
+address = user_input
+locator = Nominatim(user_agent="geoCoder")
+location = locator.geocode(address)
+
+addr_lat = location.latitude
+addr_long = location.longitude
+address_df = pd.DataFrame({'Address': [address], 
+                        'Latitude': [addr_lat], 
+                        'Longitude': [addr_long]
+                       })
+address_gdf = gpd.GeoDataFrame(address_df, geometry=gpd.points_from_xy(address_df.Longitude, address_df.Latitude))
+folium.Marker((addr_lat, addr_long), popup=address, 
+              icon=folium.Icon(color='red', icon_color='white', 
+                icon='male', angle=0, prefix='fa')).add_to(mapCville)
+
+#https://towardsdatascience.com/nearest-neighbour-analysis-with-geospatial-data-7bcd95f34c0e
+def closest_id(r, val, c="geometry"):
+    target_geom = nearest_points(r[c], nodes_gdf.unary_union)
+    target = nodes_gdf[nodes_gdf.geometry == target_geom[1]]
+    return target.index[0]
+
+bus_gdf["closest_id"] = bus_gdf.apply(closest_id, val="geometry", axis=1)
+address_gdf["closest_id"] = address_gdf.apply(closest_id, val="geometry", axis=1)
+addr_ID = address_gdf['closest_id'].to_numpy()[0]
+
+stop_ids = bus_gdf["closest_id"]
+short_len = sys.maxsize
+short_path = []
+for i in stop_ids.index:
+    try:
+        r = nx.shortest_path(G, addr_ID, stop_ids[i], weight='length')
+        len = nx.shortest_path_length(G, addr_ID, stop_ids[i], weight='length')
+        if (len < short_len):
+            short_len = len
+            short_path = r
+    except:
+        pass
+
+route_pairwise = zip(short_path[:-1], short_path[1:])
+route = [edges_gdf.loc[edge, 'geometry'].iloc[0] for edge in route_pairwise]
+#https://www.py4u.net/discuss/155637
+
+route_gdf = gpd.GeoDataFrame(route)
+route_gdf.rename( columns={0 :'geometry'}, inplace=True)
+
+route_style = {'fillColor': '#E24500', 'color': '#E24500', 'weight' : 4}
+route_json = route_gdf.to_json()
+folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapCville)
+
+target_stop = bus_gdf[bus_gdf.closest_id == short_path[-1]]
+target_union = target_stop.unary_union
+
+try:
+    for t in target_union:
+        name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
+        folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='green', icon_color='white', 
+                    icon='bus', angle=0, prefix='fa')).add_to(mapCville)
+except:
+    name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
+    folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='green', icon_color='white', 
+                    icon='bus', angle=0, prefix='fa')).add_to(mapCville)
 
 folium_static(mapCville)
 
