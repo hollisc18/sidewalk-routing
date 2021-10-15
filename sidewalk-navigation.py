22b514343c5aa08b0b85679558e891bc157435cd
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-10-15 13:39:57 -0400

    Update sidewalk-navigation.py

--- sidewalk-navigation.py
+++ sidewalk-navigation.py
@@ -3,28 +3,6 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import geopandas as gpd
-import requests
-import datetime
-import json, re
-import pydeck as pdk
-import pyproj
-from shapely.ops import orient
-import io
-import geojson
-import networkx as nx
-import osmnx as ox
-import folium
-import overpy
-import shapely
-from shapely.geometry import LineString
-from shapely.geometry import MultiPoint
-from shapely.geometry import Point
-from shapely.geometry import MultiLineString
-from shapely.ops import nearest_points
-from IPython.display import IFrame
-from geopandas.tools import geocode
-import geopy
-from geopy.geocoders import Nominatim
 
 """
 # Sidewalk navigation to the closest CAT bus stop in Charlottesville
