298ae003d19732a5c8f9f62c1045965377aa7606
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-10-15 14:51:46 -0400

    Update sidewalk-navigation.py

--- sidewalk-navigation.py
+++ sidewalk-navigation.py
@@ -53,7 +53,7 @@ nodes_gdf, edges_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry
 style = {'fillColor': '#0064A7', 'color': '#0064A7', 'weight' : 2}
 sidewalk_json = edges_gdf.to_json()
 folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
-mLoc = folium_static(mapCville)
+folium_static(mapCville)
 
 user_input = st.text_input("Enter adress", "")
 
