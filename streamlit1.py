3bfc63a4e8b867fb1ba3afc7e520693b75a636f8
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-11-02 21:07:36 -0400

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -24,7 +24,7 @@ from shapely.ops import nearest_points
 from geopandas.tools import geocode
 import geopy
 from geopy.geocoders import Nominatim
-
+import leafmap.foliumap as leafmap
 """
 # Sidewalk navigation to the closest CAT bus stop in Charlottesville
 
@@ -32,104 +32,3 @@ testing testing
 
 """
 
-#https://towardsdatascience.com/nearest-neighbour-analysis-with-geospatial-data-7bcd95f34c0e
-def closest_id(r, val, c="geometry"):
-    target_geom = nearest_points(r[c], nodes_gdf.unary_union)
-    target = nodes_gdf[nodes_gdf.geometry == target_geom[1]]
-    return target.index[0]
-
-@st.cache(suppress_st_warning=True)
-def mapCreation():
-            time.sleep(2)
-            mapCville = folium.Map(location = [38.031704,-78.490532], tiles = 'OpenStreetMap', zoom_start = 14)
-            #read bus stops and add to map
-            bus_gdf = gpd.read_file('https://opendata.arcgis.com/datasets/6465cd54bcf4498495be8c86a9d7c3f2_4.geojson')
-            bus_json = bus_gdf.to_json()
-            folium.GeoJson(bus_json, 
-                        tooltip=folium.features.GeoJsonTooltip(
-                                        fields=['StopName'], 
-                                        aliases=["Stop Name"])
-            ).add_to(mapCville)
-            #https://stackoverflow.com/questions/61136785/folium-geojsonsome-data-how-to-set-marker-type
-
-            #create graph of cville sidewalks
-            cville = "Charlottesville, Virginia, USA"
-            G = ox.graph_from_place(cville, network_type='drive')
-            #convert to geodataframe
-            sidewalk_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
-            #extract nodes and edges
-            nodes_gdf, edges_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
-
-            style = {'fillColor': '#0064A7', 'color': '#0064A7', 'weight' : 2}
-            sidewalk_json = edges_gdf.to_json()
-            folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
-            folium_static(mapCville)
-            
-            #create attribute of closest node id on the network to calculate route
-            bus_gdf["closest_id"] = bus_gdf.apply(closest_id, val="geometry", axis=1)
-
-mapCreation()
-
-user_input = st.text_input("Enter address", "155 Rugby Rd, Charlottesville, VA")
-
-address = user_input
-locator = Nominatim(user_agent="geoCoder")
-location = locator.geocode(address)
-
-addr_lat = location.latitude
-addr_long = location.longitude
-address_df = pd.DataFrame({'Address': [address],'Latitude': [addr_lat],'Longitude': [addr_long]})
-address_gdf = gpd.GeoDataFrame(address_df, geometry=gpd.points_from_xy(address_df.Longitude, address_df.Latitude))
-
-mapRoute = folium.Map(location = [addr_lat,addr_long], tiles = 'OpenStreetMap', zoom_start = 16)
-folium.Marker((addr_lat, addr_long), popup=address, 
-              icon=folium.Icon(color='red', icon_color='white', 
-                icon='male', angle=0, prefix='fa')).add_to(mapRoute)
-
-
-address_gdf["closest_id"] = address_gdf.apply(closest_id, val="geometry", axis=1)
-addr_ID = address_gdf['closest_id'].to_numpy()[0]
-
-stop_ids = bus_gdf["closest_id"]
-short_len = sys.maxsize
-short_path = []
-for i in stop_ids.index:
-    try:
-        r = nx.shortest_path(G, addr_ID, stop_ids[i], weight='length')
-        len = nx.shortest_path_length(G, addr_ID, stop_ids[i], weight='length')
-        if (len < short_len):
-            short_len = len
-            short_path = r
-    except:
-        pass
-
-route_zip = zip(short_path[:-1], short_path[1:])
-route = [edges_gdf.loc[edge, 'geometry'].iloc[0] for edge in route_zip]
-#https://www.py4u.net/discuss/155637
-
-route_gdf = gpd.GeoDataFrame(route)
-route_gdf.rename( columns={0 :'geometry'}, inplace=True)
-
-route_style = {'fillColor': '#00A9ED', 'color': '#00A9ED', 'weight' : 7}
-route_json = route_gdf.to_json()
-folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapRoute)
-
-target_stop = bus_gdf[bus_gdf.closest_id == short_path[-1]]
-target_union = target_stop.unary_union
-#if target union contains two stops (north bound and south bound) add both
-try:
-    for t in target_union:
-        name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
-        folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='green', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
-        mapRoute.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
-except:
-    name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
-    folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='green', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
-    mapRoute.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
-
-"""
-Closest stop route 
-"""
-folium_static(mapRoute)
