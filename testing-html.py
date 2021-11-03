b56f3e566b01d566c9f0b7cf71893e45e0dda2d2
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-11-03 19:11:39 -0400

    Update testing-html.py

--- testing-html.py
+++ testing-html.py
@@ -34,17 +34,43 @@ st.set_page_config(layout = 'wide')
 This application was developed for pedestrians in Charlottesville to better navigate a wheelchair accessible route to their destination.
 All data is pulled from OpenStreetMap, a collaborative project to create a free and accurate geographic database of the world. 
 Code for Charlottesville has been working with OpenStreetMap to map sidewalks, curbs, and crosswalks around Cville.
-Read more about this project at https://www.codeforcville.org/sidewalk-mapping
 """
+"""
+Interact with the map to view all of the Charlottesville bus stops in the area. 
+Enter a Charlottesville address in the sidebar to calculate the route to the closest stop and see the sidewalk maps within half a mile of the address.
+"""
+bus_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-routing/main/bus_gdf.geojson')
+
+def add_bus(m):
+    bus_union = bus_gdf.unary_union
+    for i in bus_union:
+        name = bus_gdf[bus_gdf['geometry'] == i]['StopName'].to_numpy()[0]
+        folium.Marker((i.y, i.x), popup=name, icon=folium.Icon(color='red', icon_color='white', icon='bus', angle=0, prefix='fa')).add_to(m)
+    return m
 
 @st.cache
 def map1():
     mapCAT = folium.Map(location = [38.031704,-78.490532], tiles = 'OpenStreetMap', zoom_start = 14)
-    bus_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-routing/main/bus_gdf.geojson')
-    bus_union = bus_gdf.unary_union
-    for i in bus_union:
-        name = bus_gdf[bus_gdf['geometry'] == i]['StopName'].to_numpy()[0]
-        folium.Marker((i.y, i.x), popup=name, icon=folium.Icon(color='red', icon_color='white', icon='bus', angle=0, prefix='fa')).add_to(mapCAT)    
+    mapCAT = add_bus(mapCAT)
     return mapCAT.get_root().render()
    
-components.html(map1())   
+col1, col2 = st.columns(2)
+col1.subheader("CAT Stops")
+col2.subheader("Route to Stop:")
+with col1:
+    components.html(map1())
+
+@st.cache
+def map2():
+    G = ox.graph_from_place("Charlottesville, Virginia, USA", network_type='walk')
+    #convert to geodataframe
+    sidewalk_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
+    #extract nodes and edges
+    nodes_gdf, edges_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
+    edges2_gdf = edges_gdf[edges_gdf['highway'] == 'footway']
+
+    mapCville = folium.Map(location = [38.035629,-78.503403], tiles = 'OpenStreetMap', zoom_start = 15)
+    style = {'fillColor': '#B44700', 'color': '#B44700', 'weight' : 1.5, 'opacity': 0.7}
+    sidewalk_json = edges2_gdf.to_json()
+    folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
+
