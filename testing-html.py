f22102f5a73600e17adf189d9ca9910aef361328
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-11-04 09:51:33 -0400

    Update testing-html.py

--- testing-html.py
+++ testing-html.py
@@ -70,10 +70,10 @@ def map1():
     style = {'fillColor': '#B44700', 'color': '#B44700', 'weight' : 1.5, 'opacity': 0.7}
     folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
     mapCville = add_bus(mapCville)
-    return mapCville.get_root().render()
+    return mapCville
 
 with col1:
-    components.html(map1(), height=450)
+    components.html(map1().get_root().render(), height=450)
     
 st.sidebar.subheader("Enter an address below:")
 user_input = st.sidebar.text_input("(Street, City, State Zip)", "155 Rugby Rd, Charlottesville, VA")
@@ -87,17 +87,12 @@ addr_long = location.longitude
 address_df = pd.DataFrame({'Address': [address],'Latitude': [addr_lat],'Longitude': [addr_long]})
 address_gdf = gpd.GeoDataFrame(address_df, geometry=gpd.points_from_xy(address_df.Longitude, address_df.Latitude))
 
-mapRoute = folium.Map(location = [38.035629,-78.503403], tiles = 'OpenStreetMap', zoom_start = 15)
-folium.Marker((addr_lat, addr_long), popup=address, 
-              icon=folium.Icon(color='darkblue', icon_color='white', 
-                icon='male', angle=0, prefix='fa')).add_to(mapRoute)
-
-G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf = create_graph()
-
 def closest_id(r, val, c="geometry"):
     target_geom = nearest_points(r[c], nodes_gdf.unary_union)
     target = nodes_gdf[nodes_gdf.geometry == target_geom[1]]
     return target.index[0]
+
+G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf = create_graph()
 
 CAT_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-routing/main/bus_gdf.geojson')
 
@@ -126,33 +121,41 @@ route = [edges_gdf.loc[edge, 'geometry'].iloc[0] for edge in route_pairwise]
 route_gdf = gpd.GeoDataFrame(route)
 route_gdf.rename( columns={0 :'geometry'}, inplace=True)
 
-route_style = {'fillColor': '#00E6FF', 'color': '#00E6FF', 'weight' : 6}
-route_json = route_gdf.to_json()
-folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapRoute)
-
 target_stop = bus_gdf[bus_gdf.closest_id == short_path[-1]]
 target_union = target_stop.unary_union
+global n
+@st.cache
+def mapRoute(addr_lat, addr_long, address, target_union, bus_gdf, route_gdf):
+    mapRoute = map1()
+    folium.Marker((addr_lat, addr_long), popup=address, 
+                  icon=folium.Icon(color='darkblue', icon_color='white', 
+                    icon='male', angle=0, prefix='fa')).add_to(mapRoute)
+    route_style = {'fillColor': '#00E6FF', 'color': '#00E6FF', 'weight' : 6}
+    route_json = route_gdf.to_json()
+    folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapRoute)
+    
+    name = ""
+    try:
+        for t in target_union:
+            name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
+            folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
+                        icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+            mapRoute.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
 
-name = ""
-try:
-    for t in target_union:
-        name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
-        folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
-        mapRoute.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
-
-except:
-    name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
-    folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
-    mapRoute.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
-
+    except:
+        name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
+        n=name
+        folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
+                        icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+        mapRoute.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
+    return mapRoute
+    
 with col2:
-    components.html(mapRoute.get_root().render(), height=450)
+    components.html(mapRoute(addr_lat, addr_long, address, target_union, bus_gdf, route_gdf).get_root().render(), height=450)
     
 st.sidebar.write("")
 st.sidebar.subheader("Closest stop:")    
-st.sidebar.write(name)
+st.sidebar.write(n)
 st.sidebar.write("")
 st.sidebar.write("")
 st.sidebar.write("Read more about this project at https://www.codeforcville.org/sidewalk-mapping")
