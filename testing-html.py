44798fd24a5e9324a79349e2f22b0b36cae7c49f
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-11-03 20:20:59 -0400

    Update testing-html.py

--- testing-html.py
+++ testing-html.py
@@ -96,6 +96,12 @@ addr_long = location.longitude
 address_df = pd.DataFrame({'Address': [address],'Latitude': [addr_lat],'Longitude': [addr_long]})
 address_gdf = gpd.GeoDataFrame(address_df, geometry=gpd.points_from_xy(address_df.Longitude, address_df.Latitude))
 
+mapRoute = folium.Map(location = [38.035629,-78.503403], tiles = 'OpenStreetMap', zoom_start = 15)
+
+folium.Marker((addr_lat, addr_long), popup=address, 
+              icon=folium.Icon(color='darkblue', icon_color='white', 
+                icon='male', angle=0, prefix='fa')).add_to(mapRoute)
+
 G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf = create_graph()
 def closest_id(r, val, c="geometry"):
     target_geom = nearest_points(r[c], nodes_gdf.unary_union)
@@ -106,3 +112,44 @@ bus_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-ro
 address_gdf["closest_id"] = address_gdf.apply(closest_id, val="geometry", axis=1)
 addr_ID = address_gdf['closest_id'].to_numpy()[0]
 
+G2 = G
+stop_ids = bus_gdf["closest_id"]
+short_len = sys.maxsize
+short_path = []
+for i in stop_ids.index:
+    try:
+        r = nx.shortest_path(G2, addr_ID, stop_ids[i], weight='length')
+        len = nx.shortest_path_length(G2, addr_ID, stop_ids[i], weight='length')
+        if (len < short_len):
+            short_len = len
+            short_path = r
+    except:
+        pass
+
+route_pairwise = zip(short_path[:-1], short_path[1:])
+route = [edges_gdf.loc[edge, 'geometry'].iloc[0] for edge in route_pairwise]
+route_gdf = gpd.GeoDataFrame(route)
+route_gdf.rename( columns={0 :'geometry'}, inplace=True)
+
+route_style = {'fillColor': '#00E6FF', 'color': '#00E6FF', 'weight' : 6}
+route_json = route_gdf.to_json()
+folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapRoute)
+
+target_stop = bus_gdf[bus_gdf.closest_id == short_path[-1]]
+target_union = target_stop.unary_union
+
+name = ""
+try:
+    for t in target_union:
+        name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
+        folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
+                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+        mapRoute.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
+
+except:
+    name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
+    folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
+                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+    mapRoute.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
+
+    
