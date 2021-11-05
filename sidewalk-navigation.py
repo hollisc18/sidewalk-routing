cf0b654eb3f7cb0bf1313ecbd9a24d349cf1ff31
hollisc18 <64624238+hollisc18@users.noreply.github.com>
2021-11-05 12:55:25 -0400

    Update sidewalk-navigation.py

--- sidewalk-navigation.py
+++ sidewalk-navigation.py
@@ -18,39 +18,67 @@ import folium
 import sys
 import overpy
 import shapely
-import webbrowser
 from streamlit_folium import folium_static
 import streamlit.components.v1 as components
 #https://discuss.streamlit.io/t/ann-streamlit-folium-a-component-for-rendering-folium-maps/4367
 from shapely.ops import nearest_points
 from geopandas.tools import geocode
 import geopy
+import urllib
 from geopy.geocoders import Nominatim
 
 st.set_page_config(layout = 'wide')
 
 """
 # Sidewalk Navigation to CAT Bus Stops in Charlottesville
-
 This application was developed for pedestrians in Charlottesville to better navigate a wheelchair accessible route to their destination.
 All data is pulled from OpenStreetMap, a collaborative project to create a free and accurate geographic database of the world. 
 Code for Charlottesville has been working with OpenStreetMap to map sidewalks, curbs, and crosswalks around Cville.
-
 """
 """
-Interact with the map to view all of the Charlottesville bus stops in the area. 
-Enter a Charlottesville address in the sidebar to calculate the route to the closest stop and see the sidewalk maps within half a mile of the address.
+Interact with the map to view all of the Charlottesville bus stops and sidewalk maps in the area. 
+Enter a Charlottesville address in the sidebar to calculate the route to the closest stop.
 """
 
-mapCAT = folium.Map(location = [38.031704,-78.490532], tiles = 'OpenStreetMap', zoom_start = 14)
-CAT_gdf = gpd.read_file('https://opendata.arcgis.com/datasets/6465cd54bcf4498495be8c86a9d7c3f2_4.geojson')
-CAT_union = CAT_gdf.unary_union
-for i in CAT_union:
-    name = CAT_gdf[CAT_gdf['geometry'] == i]['StopName'].to_numpy()[0]
-    folium.Marker((i.y, i.x), popup=name, icon=folium.Icon(color='red', icon_color='white', icon='bus', angle=0, prefix='fa')).add_to(mapCAT)
+def add_bus(m):
+    bus_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-routing/main/bus_gdf.geojson')
+    bus_union = bus_gdf.unary_union
+    for i in bus_union:
+        name = bus_gdf[bus_gdf['geometry'] == i]['StopName'].to_numpy()[0]
+        folium.Marker((i.y, i.x), popup=name, icon=folium.Icon(color='red', icon_color='white', icon='bus', angle=0, prefix='fa')).add_to(m)
+    return m
+   
+col1, col2 = st.columns(2)
+col1.subheader("CAT Stops")
+col2.subheader("Route to Stop:")
 
+def create_graph():
+    G = ox.graph_from_place("Charlottesville, Virginia, USA", network_type='walk')
+    #convert to geodataframe
+    sidewalk_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
+    #extract nodes and edges
+    nodes_gdf, edges_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
+    edges2_gdf = edges_gdf[edges_gdf['highway'] == 'footway']
+    return G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf
+
+
+def map1():
+    G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf = create_graph()
+    sidewalk_json = edges2_gdf.to_json()
+    mapCville = folium.Map(location = [38.035629,-78.503403], tiles = 'OpenStreetMap', zoom_start = 15)
+    style = {'fillColor': '#B44700', 'color': '#B44700', 'weight' : 1.5, 'opacity': 0.7}
+    folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
+    mapCville = add_bus(mapCville)
+    return mapCville
+
+@st.cache
+def m1Html():
+    return map1().get_root().render()
+with col1:
+    components.html(m1Html(), height=450)
+    
 st.sidebar.subheader("Enter an address below:")
-user_input = st.sidebar.text_input("(Street, City, State Zip)", "155 Rugby Rd, Charlottesville, VA")
+user_input = st.sidebar.text_input("(Street, City, State Zip)", "1215 Lee St, Charlottesville, VA 22903")
 
 address = user_input
 locator = Nominatim(user_agent="geoCoder")
@@ -61,49 +89,23 @@ addr_long = location.longitude
 address_df = pd.DataFrame({'Address': [address],'Latitude': [addr_lat],'Longitude': [addr_long]})
 address_gdf = gpd.GeoDataFrame(address_df, geometry=gpd.points_from_xy(address_df.Longitude, address_df.Latitude))
 
-col1, col2 = st.columns(2)
-col1.subheader("CAT Stops")
-with col1:
-    html_string = mapCAT.get_root().render()
-    components.html(html_string, height=450)
-
-
-col2.subheader("Route to Stop")
-
-
-#create graph of cville sidewalks
-G = ox.graph_from_address(address, 800, network_type='walk')
-#convert to geodataframe
-sidewalk_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
-#extract nodes and edges
-nodes_gdf, edges_gdf = ox.graph_to_gdfs(G, nodes=True, edges=True, node_geometry=True, fill_edge_geometry=True)
-edges2_gdf = edges_gdf[edges_gdf['highway'] == 'footway']
-
-mapCville = folium.Map(location = [addr_lat,addr_long], tiles = 'OpenStreetMap', zoom_start = 15)
-style = {'fillColor': '#B44700', 'color': '#B44700', 'weight' : 1.5, 'opacity': 0.7}
-sidewalk_json = edges2_gdf.to_json()
-folium.GeoJson(sidewalk_json, style_function=lambda x:style).add_to(mapCville)
-
-busLat = CAT_gdf[ abs(CAT_gdf['Latitude']-addr_lat) <  0.01 ] 
-bus_gdf = busLat[ abs(busLat['Longitude']-addr_long) <  0.01]
-
-bus_union = bus_gdf.unary_union
-for i in bus_union:
-    name = bus_gdf[bus_gdf['geometry'] == i]['StopName'].to_numpy()[0]
-    folium.Marker((i.y, i.x), popup=name, icon=folium.Icon(color='lightred', icon_color='white', icon='bus', angle=0, prefix='fa')).add_to(mapCville)
-
-
+mapRoute = folium.Map(location = [38.035629,-78.503403], tiles = 'OpenStreetMap', zoom_start = 15)
 folium.Marker((addr_lat, addr_long), popup=address, 
               icon=folium.Icon(color='darkblue', icon_color='white', 
-                icon='male', angle=0, prefix='fa')).add_to(mapCville)
+                icon='male', angle=0, prefix='fa')).add_to(mapRoute)
 
-#https://towardsdatascience.com/nearest-neighbour-analysis-with-geospatial-data-7bcd95f34c0e
+G, sidewalk_gdf, nodes_gdf, edges_gdf, edges2_gdf = create_graph()
+
 def closest_id(r, val, c="geometry"):
     target_geom = nearest_points(r[c], nodes_gdf.unary_union)
     target = nodes_gdf[nodes_gdf.geometry == target_geom[1]]
     return target.index[0]
 
-bus_gdf["closest_id"] = bus_gdf.apply(closest_id, val="geometry", axis=1)
+CAT_gdf = gpd.read_file('https://raw.githubusercontent.com/hollisc18/sidewalk-routing/main/bus_gdf.geojson')
+
+busLat = CAT_gdf[ abs(CAT_gdf['Latitude']-addr_lat) <  0.01 ] 
+bus_gdf = busLat[ abs(busLat['Longitude']-addr_long) <  0.01]
+
 address_gdf["closest_id"] = address_gdf.apply(closest_id, val="geometry", axis=1)
 addr_ID = address_gdf['closest_id'].to_numpy()[0]
 
@@ -128,7 +130,7 @@ route_gdf.rename( columns={0 :'geometry'}, inplace=True)
 
 route_style = {'fillColor': '#00E6FF', 'color': '#00E6FF', 'weight' : 6}
 route_json = route_gdf.to_json()
-folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapCville)
+folium.GeoJson(route_json, style_function=lambda x:route_style).add_to(mapRoute)
 
 target_stop = bus_gdf[bus_gdf.closest_id == short_path[-1]]
 target_union = target_stop.unary_union
@@ -138,20 +140,18 @@ try:
     for t in target_union:
         name = bus_gdf[bus_gdf['geometry'] == t]['StopName'].to_numpy()[0]
         folium.Marker((t.y, t.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapCville)
-        mapCville.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
+                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+        mapRoute.fit_bounds([[addr_lat,addr_long], [t.y, t.x]])
 
 except:
     name = bus_gdf[bus_gdf['geometry'] == target_union]['StopName'].to_numpy()[0]
     folium.Marker((target_union.y, target_union.x), popup=name, icon=folium.Icon(color='red', icon_color='white', 
-                    icon='bus', angle=0, prefix='fa')).add_to(mapCville)
-    mapCville.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
+                    icon='bus', angle=0, prefix='fa')).add_to(mapRoute)
+    mapRoute.fit_bounds([[addr_lat,addr_long], [target_union.y, target_union.x]])
 
-    
 with col2:
-    html_string2 = mapCville.get_root().render()
-    components.html(html_string2, height=450)
-
+    components.html(mapRoute.get_root().render(), height=450)
+    
 st.sidebar.write("")
 st.sidebar.subheader("Closest stop:")    
 st.sidebar.write(name)
